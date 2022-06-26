625165a92fa8c2c28b71aaa49ccb07b4e47bc79a
MJB <matt.berti13@gmail.com>
2022-06-25 19:57:52 -0500

    added front-end functionality and formatted the createdAt timestamps

--- models/Pizza.js
+++ models/Pizza.js
@@ -1,3 +1,4 @@
+const dateFormat = require('../utils/dateFormat');
 const { Schema, model } = require('mongoose');
 
 const PizzaSchema = new Schema({
@@ -9,7 +10,8 @@ const PizzaSchema = new Schema({
     },
     createdAt:{
         type: Date,
-        default: Date.now
+        default: Date.now,
+        get: (createdAtVal) => dateFormat(createdAtVal)
     },
     size:{
         type: String,
@@ -28,6 +30,7 @@ const PizzaSchema = new Schema({
 {
 toJSON: {
     virtuals: true,
+    getters: true
 },
 id: false
 }
